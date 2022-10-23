fb68898e19e81b219c54f78d93e7ce92ac51a18f
reem-elbakry <reemelbakry95@gmail.com>
2022-10-23 22:54:57 +0200

    intializing server

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`server running on port ${port}`));
